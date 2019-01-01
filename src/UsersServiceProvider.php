7614ee65d9a73810291538ed8c81385d1584d6ab
Sinevia <info@sinevia.com>
2019-01-01 09:36:09 +0000

    Create UsersServiceProvider.php

---
+++ src/UsersServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Sinevia\Settings;
+
+use Illuminate\Support\ServiceProvider;
+
+class UsersServiceProvider extends ServiceProvider {
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot() {
+        $this->publishes([
+            dirname(__DIR__) . '/config/users.php' => config_path('users.php'),
+            $this->loadMigrationsFrom(dirname(__DIR__) . '/database/migrations'),
+            //$this->loadViewsFrom(dirname(__DIR__) . '/resources/views', 'tasks'),
+            //$this->loadRoutesFrom(dirname(__DIR__).'/routes.php'),            
+        ]);
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register() {
+        //
+    }
+
+}
+
